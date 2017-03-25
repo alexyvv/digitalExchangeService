094fc3cf465aa9185125d2d1cc5ece46bcd09a24
Алексей <alexyvv@gmail.com>
2017-03-25 17:29:39 +0300

    For push test.

--- src/main/java/ru/openbank/digital/exchenge/ExchangeServiceApplication.java
+++ src/main/java/ru/openbank/digital/exchenge/ExchangeServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExchangeServiceApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(ExchangeServiceApplication.class, args);
 	}
 }
