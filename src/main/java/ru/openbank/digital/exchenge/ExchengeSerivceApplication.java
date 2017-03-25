04753675faf0d4f650c9aa657d331e122d68f15c
Алексей <alexyvv@gmail.com>
2017-03-25 11:55:02 +0300

    initial commit

---
+++ src/main/java/ru/openbank/digital/exchenge/ExchengeSerivceApplication.java
@@ -0,0 +1,12 @@
+package ru.openbank.digital.exchenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchengeSerivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchengeSerivceApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/openbank/digital/exchenge/ExchengeSerivceApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.openbank.digital.exchenge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExchengeSerivceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
